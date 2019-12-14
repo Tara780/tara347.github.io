2eb7eb0087e12f8adb422b4f8ea714e28d115a5b
tara347 <lyricsop19@gmail.com>
2019-12-13 20:59:10 -0500

    project - testing header grid options

--- project/js/scripts.js
+++ project/js/scripts.js
@@ -1,3 +1,10 @@
+// Toggle the hamburger menu class name upon click
+function toggleMenu() {
+  document
+    .getElementsByClassName("navigation")[4]
+    .classList.toggle("responsive");
+}
+
 // Return current year in numeric form
 const options = {year:'numeric'};
 document.getElementById('currentyear').textContent = new Date().toLocaleDateString('en-US', options);
